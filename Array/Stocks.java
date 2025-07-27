b0eb9f500e6c0b18f4a9d0d916ebf33766f83e88
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-07-27 12:36:25 +0530

    stocks

--- Array/Stocks.java
+++ Array/Stocks.java
@@ -1,21 +1,21 @@
 public class Stocks {
-    public static void Profit(int prices[]){
+    public static void Profit(int Sell_prices[]){
          int max_profit = 0;
          int buy = Integer.MAX_VALUE;
-         for ( int i = 0;i<prices.length;i++){
-            if(buy<prices[i]){
-                int profit = prices[i]-buy;
+         for ( int i = 0;i<Sell_prices.length;i++){
+            if(buy<Sell_prices[i]){
+                int profit = Sell_prices[i]-buy;
                 max_profit = Math.max(max_profit, profit);
             } else {
-                buy = prices[i];
+                buy = Sell_prices[i];
             }
 
          }
          System.out.println("Max Profit: " + max_profit);
     }
     public static void main(String[] args) {
-        int prices[] = { 7, 1, 5, 3, 6, 4 ,10 };
-        Profit(prices);
+        int Sell_prices[] = { 7, 1, 5, 3, 6, 4 ,10 };
+        Profit(Sell_prices);
         
     }
     
