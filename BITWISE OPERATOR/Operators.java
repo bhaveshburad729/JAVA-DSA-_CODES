5272d5fd5757115bddf7ebf086ee0fe5152b2c9b
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-07-28 19:29:29 +0530

    clear bits from ith to  j th range

--- BITWISE OPERATOR/Operators.java
+++ BITWISE OPERATOR/Operators.java
@@ -15,10 +15,23 @@ class Operators {
         int bitmask = 1<<i;
         return n&(~bitmask);
     }
+    public static int updateIthBit(int n , int i,int newbit){
+        if (newbit == 0){
+           return clearIthBit(n ,i );
+        }
+        else{
+           return setIthbit(n ,i);
+        }
+    }
+    public static int clearLastIBits(int n , int i){
+        int bitmask = (-1) << i; // -1 == (~0);
+        return n & bitmask;
+    }
 
     public static void main(String[] args) {
        // System.out.println(getIthbit(10, 2));
         //System.out.println(setIthbit(10, 2));
         //System.out.println(clearIthBit(10,1)); // clears the
+        System.out.println(updateIthBit(10, 2, 0)); // updates the ith bit to newbit
     }
 }
