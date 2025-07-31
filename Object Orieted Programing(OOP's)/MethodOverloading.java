aa87934d748325e476ec3f79865c631263752d9b
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-07-31 12:38:09 +0530

    Method overlading example

--- Object Orieted Programing(OOP's)/MethodOverloading.java
+++ Object Orieted Programing(OOP's)/MethodOverloading.java
@@ -1,9 +1,9 @@
 class MethodOverloading {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-       System.out.println( cal.sum(4,5));
-        System.out.println(cal.sum(1.5f,2.5f));
-        System.out.println(cal.sum(1,1,1));
+       System.out.println( cal.sum(2,4));
+        System.out.println(cal.sum(3.5f,2.5f));
+        System.out.println(cal.sum(3,4,5));
     }
 }
 
