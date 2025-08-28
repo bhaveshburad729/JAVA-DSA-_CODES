fd6670b4c868d7a6a2535fed6609c8bc4bfcc03d
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-08-28 11:48:14 +0530

    factorial of no

---
+++ Recurrsion/Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+    public static int fact(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int factt = (n * fact(n - 1));
+        return factt;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
+
+    }
+}
