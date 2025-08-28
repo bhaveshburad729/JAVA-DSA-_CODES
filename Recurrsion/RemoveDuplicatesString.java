b5ef0cf9d64c4e70cfc6b131b43e3cc5c7e12e0e
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-08-28 15:09:15 +0530

    Remove duplicte strings through recurssion

---
+++ Recurrsion/RemoveDuplicatesString.java
@@ -0,0 +1,23 @@
+public class RemoveDuplicatesString {
+    public static void removeduplicates(String str, int idx, StringBuilder newstr, boolean map[]){
+        if(idx == str.length()){
+           System.out.println(  newstr);
+           return;
+        }
+        char currchar = str.charAt(idx);
+        if(map[currchar - 'a'] == true){
+            // duplicate
+            removeduplicates(str, idx + 1, newstr, map);
+        }else{
+            map[currchar - 'a'] = true;
+           
+             removeduplicates(str, idx + 1, newstr.append(currchar), map);
+        }
+    } 
+    public static void main(String[] args) {
+        String str = "appnnacollege";
+        StringBuilder newstr = new StringBuilder("");
+        boolean map[] = new boolean[26];
+        removeduplicates(str, 0, newstr, map);
+    }
+}
