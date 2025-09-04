f874c36bccf92506bf445a141d9dd1ceb55619dc
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-09-04 13:05:12 +0530

    Print Subsets Of Given Strings

---
+++ Back Tracking/PrintSubsets.java
@@ -0,0 +1,23 @@
+class PrintSubsets {
+    public static void printSubsets(String str,int i , String ans ){
+        //base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+                return;
+            }else{
+            System.out.println(ans);
+            return;
+            }
+        }
+        // kaam 
+        //yes choice 
+        printSubsets(str, i+1, ans+str.charAt(i));
+        // no choice 
+        printSubsets(str, i+1, ans);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        printSubsets(str, 0, "");
+    }
+}
