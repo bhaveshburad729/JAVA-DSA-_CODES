d2b754782cd06e396d18e58b85b8258f5b1775fc
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-09-09 11:51:50 +0530

    formated permutation code

--- Back Tracking/PrintPermutation.java
+++ Back Tracking/PrintPermutation.java
@@ -1,18 +1,19 @@
-class PrintPermutation{
-    public static void printPermutation(String str , String ans ){
-        // base case 
-        if(str.length() == 0){
+class PrintPermutation {
+    public static void printPermutation(String str, String ans) {
+        // base case
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
         }
-        //kaam 
-        for (int i = 0;i<str.length();i++){
+        // kaam
+        for (int i = 0; i < str.length(); i++) {
             char curr = str.charAt(i);
-            String Newstr = str.substring(0,i) + str.substring(i+1);
-            printPermutation(Newstr, ans+curr);
+            String Newstr = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(Newstr, ans + curr);
         }
     }
-    public static void main (String args []){
+
+    public static void main(String args[]) {
         String str = "abc";
         printPermutation(str, "");
     }
