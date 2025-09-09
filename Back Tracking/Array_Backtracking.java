62c8018f3638158a1068efd835a4f7ba1c6ad760
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-09-09 11:52:43 +0530

    array back tracking code .java

--- Back Tracking/Array_Backtracking.java
+++ Back Tracking/Array_Backtracking.java
@@ -1,5 +1,23 @@
-class Array_Backtracking{
-    public static void main (String args [] ){
-        
+class Array_Backtracking {
+
+    // Function to print subsets
+    public static void printSubsets(int arr[], int index, String curr) {
+        // Base case: if we reach end of array
+        if (index == arr.length) {
+            System.out.println(curr);
+            return;
+        }
+
+        // Choice 1: Include the current element
+        printSubsets(arr, index + 1, curr + arr[index] + " ");
+
+        // Choice 2: Exclude the current element
+        printSubsets(arr, index + 1, curr);
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3};
+        System.out.println("All subsets of the array:");
+        printSubsets(arr, 0, "");
     }
 }
