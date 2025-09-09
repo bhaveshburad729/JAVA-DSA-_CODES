5169b2e158b22907bd2758de6a799d012e43bded
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-09-09 11:52:10 +0530

    formated Subsets code  code

--- Back Tracking/PrintSubsets.java
+++ Back Tracking/PrintSubsets.java
@@ -1,21 +1,22 @@
 class PrintSubsets {
-    public static void printSubsets(String str,int i , String ans ){
-        //base case
-        if(i == str.length()){
-            if(ans.length() == 0){
+    public static void printSubsets(String str, int i, String ans) {
+        // base case
+        if (i == str.length()) {
+            if (ans.length() == 0) {
                 System.out.println("null");
                 return;
-            }else{
-            System.out.println(ans);
-            return;
+            } else {
+                System.out.println(ans);
+                return;
             }
         }
-        // kaam 
-        //yes choice 
-        printSubsets(str, i+1, ans+str.charAt(i));
-        // no choice 
-        printSubsets(str, i+1, ans);
+        // kaam
+        // yes choice
+        printSubsets(str, i + 1, ans + str.charAt(i));
+        // no choice
+        printSubsets(str, i + 1, ans);
     }
+
     public static void main(String[] args) {
         String str = "abc";
         printSubsets(str, 0, "");
